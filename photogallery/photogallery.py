14a256b2c35c2fa6b74a50ff4dfb3738b884e9ba
JoaquinVeraOrtega <j.veraortega99@gmail.com>
2024-12-26 18:51:37 -0300

    borro rezago codigo de prueba

--- photogallery/photogallery.py
+++ photogallery/photogallery.py
@@ -144,36 +144,7 @@ data_json = {
 
 def index():
     return rx.flex(render_gallery_component(data_json), justify="center")
-    return rx.vstack(
-        # Espaciador inicial para forzar el scroll
-        rx.box(height="150vh"),  # Espacio antes del componente
-
-        # Componente animado
-        motion(
-            rx.box(
-                "Hello, I'm animated on scroll!",
-                bg="teal",
-                color="white",
-                padding="20px",
-                border_radius="10px",
-                width="fit-content",
-                text_align="center",
-                margin="auto",
-            ),
-            initial={"opacity": 0, "y": 100},  # Estado inicial (fuera de vista)
-            while_in_view={"opacity": 1, "y": 0},  # Estado al entrar en vista
-            transition={
-                "type": "keyframes",
-                "stiffness": 65,  # Reduce la rigidez para animación más lenta
-                "damping": 100,  # Suavidad del movimiento
-                "duration": 2.0,  # Duración total de la animación (segundos)
-                "delay": 0.3,  # Retardo inicial para hacer la animación más evidente
-            },
-        ),
-
-        # Espaciador final
-        rx.box(height="150vh"),  # Espacio después del componente
-    )
+    
 
 
 app = rx.App(
